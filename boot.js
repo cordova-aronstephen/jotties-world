ac1fc786d631af1a56883676e4f8492cf0cb7417
cordova-aronstephen <aronstephenscordova@gmail.com>
2025-09-25 00:54:33 +0800

    Add initial implementation in boot.js

--- boot.js
+++ boot.js
@@ -1,1 +1,17 @@
-g
+// boot.js
+export class BootScene extends Phaser.Scene {
+    constructor() {
+        super("BootScene");
+    }
+
+    preload() {
+        // Load any assets needed for menu
+        this.load.image("menuBg", "assets/menu_bg.png");
+        this.load.image("button", "assets/button.png");
+        this.load.image("buttonHover", "assets/button_hover.png");
+    }
+
+    create() {
+        this.scene.start("MenuScene");
+    }
+}
