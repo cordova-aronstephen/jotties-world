c5cfae78e45eded599074af036c288daf28f8c9f
cordova-aronstephen <aronstephenscordova@gmail.com>
2025-09-25 08:21:36 +0800

    Implement boot.js

--- boot.js
+++ boot.js
@@ -1,17 +1,36 @@
-// boot.js
+import { MenuScene } from "./menu.js";
+
 export class BootScene extends Phaser.Scene {
     constructor() {
         super("BootScene");
     }
 
     preload() {
-        // Load any assets needed for menu
-        this.load.image("menuBg", "assets/menu_bg.png");
-        this.load.image("button", "assets/button.png");
-        this.load.image("buttonHover", "assets/button_hover.png");
+        // Background & dummy buttons
+        this.load.image("menuBg", "assets/map.png");
+        this.load.image("button", "assets/jottie_1.png");
+        this.load.image("buttonHover", "assets/jottie.png");
+
+        // Menu music
+        this.load.audio("menuMusic", "assets/audio/stardew_valley_loadgame.m4a");
     }
 
     create() {
+        this.scene.add("MenuScene", MenuScene, false);
         this.scene.start("MenuScene");
     }
 }
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: "#000",
+    physics: {
+        default: "arcade",
+        arcade: { debug: false }
+    },
+    scene: [BootScene]
+};
+
+new Phaser.Game(config);
